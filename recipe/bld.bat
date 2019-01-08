889625237c9ee02d22b86842a6d17698ec775c98
Julien Schueller <schueller@phimeca.com>
2019-01-08 14:01:55 +0100

    Set CMAKE_BUILD_TYPE=Release

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,12 @@
 
 mkdir build && cd build
 
-set CMAKE_CONFIG="Release"
-
 cmake -LAH -G"NMake Makefiles"                               ^
-  -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                        ^
+  -DCMAKE_BUILD_TYPE=Release                                 ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                     ^
   -DFMILIB_INSTALL_PREFIX="%LIBRARY_PREFIX%"                 ^
    ..
 if errorlevel 1 exit 1
 
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
