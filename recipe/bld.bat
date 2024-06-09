dbd60925fe14e0bd7543da4a16aae9edcd937892
Julien Schueller <schueller@phimeca.com>
2024-06-09 11:45:38 +0200

    cmake --build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,10 @@
 
-mkdir build_ && cd build_
-
 cmake -LAH -G"NMake Makefiles"                               ^
   -DCMAKE_BUILD_TYPE=Release                                 ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                     ^
   -DFMILIB_INSTALL_PREFIX="%LIBRARY_PREFIX%"                 ^
-   ..
+   -B build_ .
 if errorlevel 1 exit 1
 
-cmake --build . --config Release --target install
+cmake --build build_ --config Release --target install
 if errorlevel 1 exit 1
