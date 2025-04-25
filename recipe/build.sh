054dd366777cd512883daedeee89079b5b91e0f7
Julien Schueller <schueller@phimeca.com>
2025-04-25 10:36:23 +0200

    CMAKE_LIBTOOL

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 # Get an updated config.sub and config.guess
 #cp $BUILD_PREFIX/share/gnuconfig/config.* ./ThirdParty/Expat/expat-2.4.8/conftools
 
+sed -i.bak "s|/usr/bin/libtool|\${CMAKE_LIBTOOL}|g" Config.cmake/mergestaticlibs.cmake
+
 cmake ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
