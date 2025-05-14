2744969bd6eb02c61efa5b8dd3f0c1c94de8683c
Julien Schueller <schueller@phimeca.com>
2025-05-14 09:52:47 +0200

    Try master

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,15 @@
 
+
+git clone https://github.com/modelon-community/fmi-library.git
+cd fmi-library
+
+
 cmake -LAH -G Ninja                                         ^
   -DCMAKE_BUILD_TYPE=Release                                ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%"                    ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                 ^
   -DFMILIB_BUILD_TESTS=OFF                                  ^
+  -DFMILIB_EXTERNAL_LIBS=ON                                 ^
    -B build_ .
 if errorlevel 1 exit 1
 
